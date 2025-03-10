2498fa7c6a412caa9c026edfe42dd400b0bd60ab
michal7477 <mwaw99@wp.pl>
2025-03-10 14:53:21 +0100

    lab

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 
 public class Main {
     public static void main(String[] args) {
-
-
+        Point p = new Point(3,5);
+        p.setX(30);
+        p.setY(50);
+        p.getX();
+        p.getY();
     }
 }
